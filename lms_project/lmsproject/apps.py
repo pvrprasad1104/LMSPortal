107a73a43d7580f5f5b37a1331b5130af0955516
PVR Prasad <pvrprasad1104@gmail.com>
2026-05-17 19:37:44 +0530

    Initial commit2

---
+++ lms_project/lmsproject/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LmsprojectConfig(AppConfig):
+    name = 'lmsproject'
--- lms_project/lmsproject/settings.py
+++ lms_project/lmsproject/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "dashboard",
     "enrollments",
     "quizzes",
-    "assignments",
+    "assignments"
 ]
 
 MIDDLEWARE = [
